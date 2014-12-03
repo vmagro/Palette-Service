e9ffaeddf9d2b12390f66280112a1e0de6b28b62
Vinnie Magro <vmagro@socaldevs.com>
2014-12-02 23:51:50 -0800

    Redirect to project github page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,25 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-public class Main {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Main extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("https://github.com/vmagro/Palette-Service");
+    }
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()), "/");
         context.addServlet(new ServletHolder(new PaletteServlet()), "/palette/*");
         server.start();
         server.join();
